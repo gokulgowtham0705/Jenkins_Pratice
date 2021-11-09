044114aafeb83edafbf9671249bec694cebd426c
gokulgowtham0705 <89913178+gokulgowtham0705@users.noreply.github.com>
2021-11-09 17:39:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello Gokul welcome to jenkins");  
+     System.out.println("Hello Gokul welcome to jenkins");
+        System.out.println("learned jebkins push");
     }  
 }  
